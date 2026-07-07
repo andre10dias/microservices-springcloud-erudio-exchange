c28ae0c0148ef9816719a200743e3cb8a606f084
Carlos Dias <andre10dias@ymail.com>
2026-07-07 11:26:29 -0300

    Criando base de dados, adicionando dependências e migrations do flyway, e ajustando configs no application.yml

---
+++ exchange-service/src/main/resources/db/migration/V2__insert_exchange_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO exchange (
+    from_currency,
+    to_currency,
+    conversion_factor
+)
+VALUES
+    ('USD', 'BRL', 5.13),
+    ('USD', 'EUR', 0.88),
+    ('USD', 'GBP', 0.75),
+    ('USD', 'ARS', 1483.45),
+    ('USD', 'CLP', 929.00),
+    ('USD', 'COP', 3429.00),
+    ('USD', 'MXN', 17.53);
